ac3d3483d7aa466b093cd89cad762260a33bb527
David Serna <59783828+dsernag@users.noreply.github.com>
2020-05-29 09:12:54 -0500

    Create hello.c

---
+++ pset1/hello/hello.c
@@ -0,0 +1,10 @@
+//Ask your name an print Hello, 'Your name'
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What's your name?\n");
+    printf("Hello, %s\n", name);
+}
