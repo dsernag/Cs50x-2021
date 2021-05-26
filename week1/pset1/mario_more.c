1174766598902bf59d7a25695755be47075af4ea
David Serna Gutiérrez <dsernag@unal.edu.co>
2021-05-25 21:50:37 -0500

    New

---
+++ week1/pset1/mario_more.c
@@ -0,0 +1,34 @@
+//Double mario columns
+
+#include <stdio.h>
+#include <cs50.h>
+
+void print(char c, int h);
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    } while (h < 1 || h > 8);
+    
+    for(int i = 0; i < h; i++)
+    {
+        print(' ', h - 1 -i);
+        print('#', i+1);
+        print(' ', 2);
+        print('#', i + 1);
+        printf("\n");
+    }
+    
+}
+
+// Function to print some character i times
+
+void print(char c, int h){
+    for (int i = 0; i < h; i++)
+    {
+        printf("%c", c);
+    }
+}
---
+++ week1/readme.md
@@ -0,0 +1,6 @@
+# Week 1: [C](https://cs50.harvard.edu/x/2021/weeks/1/)
+
+Here are the solutions to the Week 1:
+    * Lab 1 Hello : (hello.c)[] 
+
+**PS:** Keeping in mind the course's policy on **[Academic Honesty](https://cs50.harvard.edu/x/2020/syllabus/#academic-honesty)**
